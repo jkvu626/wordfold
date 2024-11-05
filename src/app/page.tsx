eef82a8e044d1676707306b92f72f1d6ee6d586c
Jesse Vu <jessevu@gmail.com>
2024-11-05 12:25:27 -0500

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,118 @@
-import Image from "next/image";
+'use client'                              // directive to clarify client-side. Place at top of ALL .tsx files
+import React from 'react'
+
+import { Coordinate, Model, Square } from '../model'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // initial instantiation of the Model comes from the first configuration
+  const [model, setModel] = React.useState(new Model(0))
+  const [redraw, forceRedraw] = React.useState(0)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // helper function that forces React app to redraw whenever this is called.
+  function andRefreshDisplay() {
+    forceRedraw(redraw + 1)
+  }
+
+  // ((target.row + 1) === (selected.row))
+  function isValidMove(selected:Coordinate, target:Coordinate) {
+    if ((((selected.row - 1) === (target.row)) && (selected.column === target.column) // UP
+    || (((selected.row + 1) === (target.row)) && (selected.column === target.column)) // DOWN
+    || ((selected.row === target.row) && ((selected.column - 1) === target.column)) // LEFT
+    || ((selected.row === target.row) && ((selected.column + 1) === target.column)))) // RIGHT
+    { 
+      return true
+    } else {
+      return false
+    }
+  }
+
+  
+  function fold(row:number, column:number) {
+    if (model.board.selectedSquare && (model.contents(row, column) != '')) {
+      const selectedRow = model.board.selectedSquare.row
+      const selectedCol = model.board.selectedSquare.column
+      const selectedCoord = new Coordinate(selectedRow, selectedCol)
+      const targetCoord = new Coordinate(row, column)
+      const selectedContents = model.contents(selectedRow, selectedCol)
+      if (isValidMove(selectedCoord, targetCoord)) {
+        model.setcontents(selectedRow, selectedCol, '') 
+        return selectedContents
+      } else {
+        return ''
+      }
+    }
+    return ''
+  }
+
+  function handleClick(row:number, column:number) {
+    let selected = new Coordinate(row, column)
+    if (model.board.selectedSquare) {
+      const selectedRow = model.board.selectedSquare.row
+      const selectedCol = model.board.selectedSquare.column
+      const selectedCoord = new Coordinate(selectedRow, selectedCol)
+      const targetCoord = new Coordinate(row, column)
+      const combinedString = fold(row,column) + model.contents(row, column) 
+      if (isValidMove(selectedCoord, targetCoord) && model.contents(row, column) != '' && (combinedString.length <= 6)){
+        model.setcontents(row, column, combinedString)
+        model.board.selectedSquare = selected
+    } else if (model.contents(row,column) != '') {
+      model.board.selectedSquare = selected
+    }
+    andRefreshDisplay()
+  }
+
+  // change the style for the given square based on model. Space separated string.
+  // So "square" is a regular square, while "square selected" is a selected square. Find
+  // these CSS definitions inside the global.css file
+  function css(row:number, column:number) {
+    if ((row === model.board.selectedSquare?.row) && (column === model.board.selectedSquare.column))  {
+      return "square selected"
+    }
+    return "square"
+  }
+
+  return (
+    <div>
+      <div className="board">
+        <div className="button-container">
+          <button data-testid="0,0" className={css(0,0)} onClick={() => handleClick(0, 0)}>{model.contents(0,0)}</button>
+          <button data-testid="0,1" className={css(0,1)} onClick={() => handleClick(0, 1)}>{model.contents(0,1)}</button>
+          <button data-testid="0,2" className={css(0,2)} onClick={() => handleClick(0, 2)}>{model.contents(0,2)}</button>
+          <button data-testid="0,3" className={css(0,3)} onClick={() => handleClick(0, 3)}>{model.contents(0,3)}</button>
+          <button data-testid="0,4" className={css(0,4)} onClick={() => handleClick(0, 4)}>{model.contents(0,4)}</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="button-container">
+          <button data-testid="1,0" className={css(1,0)} onClick={() => handleClick(1, 0)}>{model.contents(1,0)}</button>
+          <button data-testid="1,1" className={css(1,1)} onClick={() => handleClick(1, 1)}>{model.contents(1,1)}</button>
+          <button data-testid="1,2" className={css(1,2)} onClick={() => handleClick(1, 2)}>{model.contents(1,2)}</button>
+          <button data-testid="1,3" className={css(1,3)} onClick={() => handleClick(1, 3)}>{model.contents(1,3)}</button>
+          <button data-testid="1,4" className={css(1,4)} onClick={() => handleClick(1, 4)}>{model.contents(1,4)}</button>
+        </div>
+        <div className="button-container">
+          <button data-testid="2,0" className={css(2,0)} onClick={() => handleClick(2, 0)}>{model.contents(2,0)}</button>
+          <button data-testid="2,1" className={css(2,1)} onClick={() => handleClick(2, 1)}>{model.contents(2,1)}</button>
+          <button data-testid="2,2" className={css(2,2)} onClick={() => handleClick(2, 2)}>{model.contents(2,2)}</button>
+          <button data-testid="2,3" className={css(2,3)} onClick={() => handleClick(2, 3)}>{model.contents(2,3)}</button>          
+          <button data-testid="2,4" className={css(2,4)} onClick={() => handleClick(2, 4)}>{model.contents(2,4)}</button>
+        </div>
+        <div className="button-container">
+          <button data-testid="3,0" className={css(3,0)} onClick={() => handleClick(3, 0)}>{model.contents(3,0)}</button>
+          <button data-testid="3,1" className={css(3,1)} onClick={() => handleClick(3, 1)}>{model.contents(3,1)}</button>
+          <button data-testid="3,2" className={css(3,2)} onClick={() => handleClick(3, 2)}>{model.contents(3,2)}</button>
+          <button data-testid="3,3" className={css(3,3)} onClick={() => handleClick(3, 3)}>{model.contents(3,3)}</button>
+          <button data-testid="3,4" className={css(3,4)} onClick={() => handleClick(3, 4)}>{model.contents(3,4)}</button>
+        </div>
+        <div className="button-container">
+          <button data-testid="4,0" className={css(4,0)} onClick={() => handleClick(4, 0)}>{model.contents(4,0)}</button>
+          <button data-testid="4,1" className={css(4,1)} onClick={() => handleClick(4, 1)}>{model.contents(4,1)}</button>
+          <button data-testid="4,2" className={css(4,2)} onClick={() => handleClick(4, 2)}>{model.contents(4,2)}</button>
+          <button data-testid="4,3" className={css(4,3)} onClick={() => handleClick(4, 3)}>{model.contents(4,3)}</button>
+          <button data-testid="4,4" className={css(4,4)} onClick={() => handleClick(4, 4)}>{model.contents(4,4)}</button>
+        </div>
+      </div>
+     
+      <label className="score">{"Score: " + "GOES HERE"}</label>
+      <label className="numMoves">{"Number of Moves: " + "GOES HERE"}</label>
     </div>
-  );
+  )
 }
---
+++ src/model.ts
@@ -0,0 +1,72 @@
+
+import { config1, config2, config3 } from './puzzle'
+
+export class Coordinate {
+    readonly row : number
+    readonly column : number
+
+    constructor(row:number, column:number) {
+      this.row = row
+      this.column = column
+    }
+}
+
+export class Square {
+    location : Coordinate
+    letters : string
+
+    constructor(location:Coordinate, letters:string) {
+        this.location = location
+        this.letters = letters 
+    }
+}
+
+export class Board {
+    squares : Square[][]
+    selectedSquare : Coordinate | undefined
+
+    constructor() {
+        this.squares = []
+        for (let r:number = 0; r < 5; r++) {
+            this.squares[r] = []
+            for (let c:number = 0; c < 5; c++) {
+                let coord = new Coordinate(r, c)
+                this.squares[r][c] = new Square(coord, '')
+            }
+        }
+
+        this.selectedSquare = undefined
+    }
+}
+
+export class Model {
+    words : string[]
+    board : Board
+    readonly configs = [ config1, config2, config3]
+    chosen : number
+
+    /** which is zero-based. */
+    constructor(which:number) {
+        this.chosen = which
+        let puzzle =  this.configs[this.chosen]
+        let board = new Board()
+        this.words = []
+        for (let r:number = 0; r < 5; r++) {
+            this.words[r] = puzzle.words[r]
+
+            for (let c:number = 0; c < 5; c++) {
+                board.squares[r][c].letters = puzzle.initial[r][c]
+            }
+        }
+        this.board = board
+    }
+
+    contents(row:number, column:number) {
+        return this.board.squares[row][column].letters
+    }
+
+    setcontents(row:number, column:number, newstring:string) {
+        this.board.squares[row][column].letters = newstring
+    }
+
+}
